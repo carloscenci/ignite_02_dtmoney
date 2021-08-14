749e92b5355e8901fd01820f66681dbbac13c87f
carloscenci <carlosc.estevam@gmail.com>
2021-08-14 14:44:34 -0300

    Init form styled

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
 import Modal from 'react-modal'
-import { Container } from '../Summary/styles'
+import { Container } from './styles'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -8,13 +8,29 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
     return (
-        <Container>
-            <Modal 
-                isOpen={isOpen} 
-                onRequestClose={onRequestClose}
+        <Modal 
+            isOpen={isOpen} 
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
             >
+            <Container>
                 <h2>Cadastrar transação</h2>
-            </Modal>
-        </Container>
+
+                <input
+                    placeholder="Título"
+                />
+                <input
+                    type="number"
+                    placeholder="Valor"
+                />
+                <input
+                    placeholder="Categoria"
+                />
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
+        </Modal>
     )
 }
