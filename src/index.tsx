b2034144d1dfed975e6d5866c88b5637f275e5a3
carloscenci <carlosc.estevam@gmail.com>
2021-08-14 19:46:36 -0300

    List seeds and transactions

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { createServer, Model } from 'miragejs';
+// import { type } from 'os';
 
 createServer({
   models: {
     transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions : [
+        {
+          id: 1,
+          title: 'Freelance de Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-08-13 15:20:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-08-14 14:30:00')
+        },
+      ]
+    })
   },
 
   routes() {
